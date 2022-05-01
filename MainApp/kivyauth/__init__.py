4b80aac58e175ab9090cfb283b7bd1d2cc9d8f81
Ashutosh <ashutosh2gudu@gmail.com>
2022-05-01 09:47:21 +0530

    added kivyauth

---
+++ MainApp/kivyauth/__init__.py
@@ -0,0 +1,9 @@
+from kivy.logger import Logger
+from kivy.utils import platform
+
+__version__ = "2.3.2"
+_log_message = "KivyAuth:" + f" {__version__}" + f' (installed at "{__file__}")'
+
+__all__ = ("login_providers", "auto_login")
+
+Logger.info(_log_message)
